25d23c6182760769519fa9bf7d65da60859241c7
Will Harris <willharris0407@gmail.com>
2020-11-10 22:38:27 -0500

    Add map class

--- map.js
+++ map.js
@@ -1,5 +1,17 @@
 const random = require('./random')
 
+/**
+ * Represents a map and contains some helper functions.
+ * @param {GridSquare[][]} arr the array containing the map's data
+ */
+class GameMap {
+    constructor(arr) {
+        this.arr = arr
+    }
+}
+
+module.exports = GameMap
+
 class Biome {
     /**
      * @param {string} name the name of the biome.
@@ -37,7 +49,7 @@ module.exports.GridSquare = GridSquare
  * Generates a new biome map and returns it.
  * @param {number} sizeX 
  * @param {number} sizeY
- * @returns {Promise<GridSquare[][]>} a promise to the completed biome map.
+ * @returns {Promise<GameMap>} a promise to the completed biome map.
  */
 module.exports.generateMap = async function(sizeX, sizeY) {
     let val = []
@@ -50,15 +62,14 @@ module.exports.generateMap = async function(sizeX, sizeY) {
         }
         val.push(row)
     }
-    return val
+    return new GameMap(val)
 }
 
 /**
  * Converts a map into a string of emojis to render the map for Discord.
- * @param {GridSquare[][]} map the map to render.
  */
-module.exports.render = function(map) {
-    return map.reduce((x0, x) => {
+module.exports.prototype.render = function() {
+    this.arr.reduce((x0, x) => {
         return x0 + x.reduce((y0, y) => {
             return y0 + '\\' + y.biome.emoji
         }, '') + '\n'
@@ -67,33 +78,32 @@ module.exports.render = function(map) {
 
 /**
  * Sets a position on a map as a player's capital if the position is valid.
- * @param {GridSquare[][]} map the map to assign capitals to.
  * @param {string} player the players to assign a capital for.
  * @param {number} x the x position of the new capital.
  * @param {number} y the y position of the new capital.
  * @returns {boolean} true if the capital is in a valid position and was
  * placed, false if the capital's position is invalid.
  */
-module.exports.assignCapital = function(map, player, x, y) {
+module.exports.prototype.assignCapital = function(player, x, y) {
     
     const distance = 2 // King move limit for capitals
 
     // Gets the length of the y
-    const lenY = map.map(arr => arr.length).reduce((a, b) => Math.min(a, b))
+    const lenY = this.arr.map(arr => arr.length).reduce((a, b) => Math.min(a, b))
     
     // Bounds checks
     const lowX = Math.max(x - distance, 0)
-    const hiX = Math.min(x + distance + 1, map.length)
+    const hiX = Math.min(x + distance + 1, this.arr.length)
     const lowY = Math.max(y - distance, 0)
     const hiY = Math.min(y + distance + 1, lenY)
 
     // Checks the squares at and surrounding the potential capital
-    if (map.slice(lowX, hiX).every(arr => {
+    if (this.arr.slice(lowX, hiX).every(arr => {
         return arr.slice(lowY, hiY).every(sq => {
             return sq.capital === null
         })
     })) {
-        map[x][y].capital = player
+        this.arr[x][y].capital = player
         return true
     } else return false
 }
