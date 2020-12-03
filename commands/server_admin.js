41fdf87db297fc228ce40ebddf600262203022e4
Will Harris <willharris0407@gmail.com>
2020-12-02 22:27:29 -0500

    Refactor server_admin.js to reflect season changes
    The previous changes were primarily so I could remove all the channel
    code from commands and put it in a more fitting place. Now that these
    changes are made, it's time to delete the channel code from the
    commands.

--- commands/server_admin.js
+++ commands/server_admin.js
@@ -1,7 +1,6 @@
 const Command = require('./Command.js')
 const data = require('../data.js')
-const GameMap = require('../map.js')
-const util = require('../util.js')
+const SeasonManager = require('../season_manager.js')
 
 /**
  * The commands that only an administrator of a Discord server can execute.
@@ -13,13 +12,6 @@ module.exports = {
      * the game. This starts a new game.
      */
     setup: new Command(async function(argv, sender, guild, channel) {
-
-        // First, let's fetch the data.
-        try {
-            var dat = await data.get()
-        } catch (err) {
-            return 'Could not start season: unable to fetch player data.'
-        }
 
         let category_name = argv[1]
         if (category_name === undefined) {
@@ -36,105 +28,31 @@ module.exports = {
             return 'Y value for map size is invalid.'
         }
 
-        // Start category creation
-        let pCategory = guild.channels.create(category_name, { 
-            type: 'category', 
-        })
-
         // Generate possible error messages
         const permissionErr
             = "I don't have channel creation permissions."
         const otherErr
             = "Something went wrong creating the category."
-        pCategory.catch(err => {
+
+        const season = new SeasonManager(
+            guild, category_name, mapSizeX, mapSizeY)
+        data.add(season)
+        
+        // Create the season, but with extra error handling
+        const seasonCreator = season.create()
+
+        // Generate the output message
+        return seasonCreator.then(() => {
+            return 'All set! Go sign up for the next season on '
+                + `<#${season.channels.signups.channel.id}>!`
+        }, err => {
             console.error(err)
+              
             if (err instanceof Error && err.message === 'Missing Permissions') {
                 return permissionErr
             }
             return otherErr
         })
-
-        // Wait for the category to be created or for it to fail
-        let category = await pCategory
-        
-        // Now actually check if the error actually happened
-        // String means it's an error (I hope)
-        if (typeof category === 'string') {
-            return 'Season start failed: ' + category
-        }
-
-        // At this point, category is all good
-        // Now the role
-        // Start role creation
-        let pRole = guild.roles.create({
-            data: {
-                name: 'Season ' + category_name
-            },
-            reason: 'Start of Season ' + category_name
-        })
-
-        // Generate possible error messages
-        pRole.catch(err => {
-            console.error(err)
-            if (err instanceof Error && err.message === 'Missing Permissions') {
-                return permissionErr
-            }
-            return otherErr
-        })
-
-        // Wait for the role to be created or for it to fail
-        let role = await pRole
-        
-        // Now actually check if the error actually happened
-        // String means it's an error (I hope)
-        if (typeof role === 'string') {
-
-            // Delete the category; it's bad now
-            category.delete()
-
-            return 'Season start failed: ' + role
-        }
-
-        // Category and role were created! Swell! Let's save them.
-        dat.playerRole = role.id
-        dat.channels.parent = category.id
-
-        // Create the map's residence channel
-        let mapChannelP = guild.channels.create('map')
-            .then(mapChannel => mapChannel.setParent(category))
-        mapChannelP.then(mapChannel => dat.channels.map = mapChannel.id)
-
-        // Create the map
-        let mapGeneratorP = GameMap.generateMap(mapSizeX, mapSizeY)
-        mapGeneratorP.then(mp => dat.map = mp)
-
-        // Wait for both the channel and the map to generate
-        // Then render the map on the map channel
-        Promise.all([mapChannelP, mapGeneratorP]).then(promises => {
-            let channel = promises[0]
-            let mp = promises[1]
-
-            // If any error happens, all the following promises reject.
-            util.chunkMessage(mp.render(), content => channel.send(content))
-                .catch(err => {
-                    console.error("Unable to fully send map.")
-                    console.error(err)
-                    channel.send('[ Error in sending map ]')
-
-                        // At this point...really? Come on!
-                        .catch(console.error)
-                })
-        })
-
-        // Lastly, let's create the signups channel.
-        let signups = await guild.channels.create('signups')
-        signups.setParent(category)
-            .then(() => dat.channels.signups = signups.id)
-
-        // Save the name of the season
-        dat.name = category_name
-
-        return `All set! Go sign up for the next season on <#${signups.id}>!`
     }),
 
     /**
@@ -145,24 +63,12 @@ module.exports = {
         switch (argv[1]) {
             case 'delete':
                 try {
-                    var dat = await data.get()
+                    await (await data.get(argv[2])).delete()
+                    return `Season ${argv[2]} deleted.`
                 } catch (err) {
+                    console.error(err)
                     return 'Cannot fetch season data.'
                 }
-
-                // Delete the signups channel
-                let signupP = 
-                    guild.channels.cache.get(dat.channels.signups).delete()
-                let parentP =
-                    guild.channels.cache.get(dat.channels.parent).delete()
-                let mapP =
-                    guild.channels.cache.get(dat.channels.map).delete()
-                let roleP =
-                    guild.roles.cache.get(dat.playerRole).delete()
-                await Promise.all([signupP, parentP, mapP, roleP])
-                return 'All season assets have been deleted.'
-            default:
-                return 'Please specify a valid way of disposing of assets.'
         }
     })
 }
